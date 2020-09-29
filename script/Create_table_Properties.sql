73a4231ba37bb12da4360d9e37f8c0aee7b3d860
mosesj <moses.joel@htcindia.com>
2020-09-29 15:15:16 +0530

    Adding Propertiy file script

--- script/Create_table_Properties.sql
+++ script/Create_table_Properties.sql
@@ -28,3 +28,27 @@ PRIMARY KEY ( properties_id )
 
 );
 
+
+CREATE TABLE IF NOT EXISTS ims_location_t (
+
+ims_location_id INT NOT NULL AUTO_INCREMENT,
+
+ims_location_code varchar(45) NOT NULL UNIQUE,
+
+ims_location_name varchar(255) NOT NULL, 
+
+description  varchar(255) ,
+
+active char(1)  NOT NULL,
+
+created_by varchar(255) NOT NULL,
+
+created_date timestamp NOT NULL ,
+
+updated_by  varchar(255) NOT NULL ,
+
+updated_date timestamp NOT NULL,
+
+PRIMARY KEY ( ims_location_id )
+
+);
