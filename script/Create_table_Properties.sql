fd1f9286bd36e7be3335fed549a32640a78591c8
mosesjoel <moses.joel@htcindia.com>
2020-09-29 15:48:19 +0530

    Update Create_table_Properties.sql

--- script/Create_table_Properties.sql
+++ script/Create_table_Properties.sql
@@ -21,3 +21,32 @@ updated_date timestamp NOT NULL,
 PRIMARY KEY ( ims_location_id )
 
 );
+
+CREATE TABLE IF NOT EXISTS recap.scsb_properties_t (
+
+scsb_properties_id INT NOT NULL AUTO_INCREMENT,
+
+p_key varchar (255) NOT NULL ,
+
+p_value varchar(1500) NOT NULL  ,
+
+description  varchar(255) ,
+
+institution_code  varchar(50) ,
+
+ims_location_code varchar(50) ,
+
+active char(1)  NOT NULL,
+
+created_date timestamp NOT NULL ,
+
+updated_date timestamp ,
+
+created_by  varchar(255) NOT NULL,
+
+updated_by  varchar(255) ,
+
+PRIMARY KEY ( scsb_properties_id )
+
+);
+
