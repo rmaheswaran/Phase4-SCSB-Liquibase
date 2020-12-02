02f893b504a31717d236e7ae3d20f4da95a2b9da
mosesjoel <moses.joel@htcindia.com>
2020-12-02 10:14:19 +0530

    Create Sprint2_imslocation_script.sql

---
+++ script/Sprint2_imslocation_script.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset Moses:2
+
+use recap;
+
+INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
+VALUES ('RECAP', 'RECAP_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
+INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
+VALUES ('HD', 'HARVARD_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
+
+UPDATE ITEM_T SET IMS_LOCATION_ID = 1;
