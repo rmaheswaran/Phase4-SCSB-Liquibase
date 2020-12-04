bc8bacd6d968c7e66e3c313a74023b2130d126c3
mosesjoel <moses.joel@htcindia.com>
2020-12-04 11:26:12 +0530

    Update Sprint2_Alter_table.sql

--- script/Sprint2_Alter_table.sql
+++ script/Sprint2_Alter_table.sql
@@ -3,6 +3,8 @@
 
 use recap;
 
+--changeset Moses:2
+
 SET FOREIGN_KEY_CHECKS=0;
 
 ALTER TABLE item_t ADD  ims_location_id  INT Not Null ,
@@ -11,5 +13,10 @@ ADD FOREIGN KEY (ims_location_id) REFERENCES ims_location_t(ims_location_id);
 SET FOREIGN_KEY_CHECKS=1;
 
 ALTER TABLE Institution_t ADD  ils_protocol varchar(10) Not Null;
+
+
+--changeset Moses:5
+
+UPDATE ITEM_T SET IMS_LOCATION_ID = 1;
 
 
