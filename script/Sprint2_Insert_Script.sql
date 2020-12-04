aed28cc9215287e8281535dc7b604ba19c7615d8
mosesjoel <moses.joel@htcindia.com>
2020-12-04 11:24:52 +0530

    Update Sprint2_Insert_Script.sql

--- script/Sprint2_Insert_Script.sql
+++ script/Sprint2_Insert_Script.sql
@@ -1,12 +1,11 @@
 --liquibase formatted sql
---changeset Moses:3
+
 
 use recap;
 
-INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
-VALUES ('RECAP_TEST', 'RECAP_LAS_TEST', '', 'Y', 'Script', NOW(), 'Script', NOW());
-
---changeset Moses:4
+--changeset Moses:3
 
 INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
-VALUES ('HD_TEST', 'HARVARD_LAS_TEST', '', 'Y', 'Script', NOW(), 'Script', NOW());
+VALUES ('RECAP', 'RECAP_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
+INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
+VALUES ('HD', 'HARVARD_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
