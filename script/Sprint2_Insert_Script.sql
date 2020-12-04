16b8bd33b3cecfec846efaa1f816efaf1c50cfb6
mosesjoel <moses.joel@htcindia.com>
2020-12-04 11:42:34 +0530

    Update Sprint2_Insert_Script.sql

--- script/Sprint2_Insert_Script.sql
+++ script/Sprint2_Insert_Script.sql
@@ -3,7 +3,7 @@
 
 use recap;
 
---changeset Moses:3
+--changeset Moses:1
 
 INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
 VALUES ('RECAP', 'RECAP_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
