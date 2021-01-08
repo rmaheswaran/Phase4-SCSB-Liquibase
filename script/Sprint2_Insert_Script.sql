a2ab5c1f880e65ea91f6cb496045c6b0b2ea6620
mosesjoel <moses.joel@htcindia.com>
2021-01-08 11:50:37 +0530

    Update Sprint2_Insert_Script.sql

--- script/Sprint2_Insert_Script.sql
+++ script/Sprint2_Insert_Script.sql
@@ -10,7 +10,7 @@ VALUES ('RECAP', 'RECAP_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
 INSERT INTO `ims_location_t`(`ims_location_code`,`ims_location_name`,`description`,`active`,`created_by`,`created_date`,`updated_by`,`updated_date`)
 VALUES ('HD', 'HARVARD_LAS', '', 'Y', 'Script', NOW(), 'Script', NOW());
         
- --changeset Rajesh:2   
+--changeset Rajesh:2   
 INSERT INTO `generic_patron_t` (`generic_patron_id`, `requesting_inst_id`, `item_own_inst_id`, `edd_generic_patron`, `retrieval_generic_partron`, `created_by`, `created_date`, `updated_by`, `updated_date`) VALUES ('1', '1', '1', '198572131', '', 'Script', '2020-09-25 00:00:00', 'Script', '2020-09-25 00:00:00');
 INSERT INTO `generic_patron_t` (`generic_patron_id`, `requesting_inst_id`, `item_own_inst_id`, `edd_generic_patron`, `retrieval_generic_partron`, `created_by`, `created_date`, `updated_by`, `updated_date`) VALUES ('2', '2', '1', '198572132', '198572368', 'Script', '2020-09-25 00:00:00', 'Script', '2020-09-25 00:00:00');
 INSERT INTO `generic_patron_t` (`generic_patron_id`, `requesting_inst_id`, `item_own_inst_id`, `edd_generic_patron`, `retrieval_generic_partron`, `created_by`, `created_date`, `updated_by`, `updated_date`) VALUES ('3', '3', '1', '198572133', '198572369', 'Script', '2020-09-25 00:00:00', 'Script', '2020-09-25 00:00:00');
@@ -21,7 +21,7 @@ INSERT INTO `generic_patron_t` (`generic_patron_id`, `requesting_inst_id`, `item
 INSERT INTO `generic_patron_t` (`generic_patron_id`, `requesting_inst_id`, `item_own_inst_id`, `edd_generic_patron`, `retrieval_generic_partron`, `created_by`, `created_date`, `updated_by`, `updated_date`) VALUES ('8', '1', '3', '23333090796010', '70620957062095', 'Script', '2020-09-25 00:00:00', 'Script', '2020-09-25 00:00:00');
 INSERT INTO `generic_patron_t` (`generic_patron_id`, `requesting_inst_id`, `item_own_inst_id`, `edd_generic_patron`, `retrieval_generic_partron`, `created_by`, `created_date`, `updated_by`, `updated_date`) VALUES ('9', '2', '3', '23333090796028', '70620917062091', 'Script', '2020-09-25 00:00:00', 'Script', '2020-09-25 00:00:00');
 
- --changeset Hari:3
+--changeset Hari:3
 INSERT INTO `job_param_data_t` (`PARAM_NAME`, `PARAM_VALUE`, `RECORD_NUM`) VALUES ('imsDepositoryCodes', 'RECAP', '2');
 INSERT INTO `job_param_data_t` (`PARAM_NAME`, `PARAM_VALUE`, `RECORD_NUM`) VALUES ('imsDepositoryCodes', 'RECAP', '3');
 INSERT INTO `job_param_data_t` (`PARAM_NAME`, `PARAM_VALUE`, `RECORD_NUM`) VALUES ('imsDepositoryCodes', 'RECAP', '4'); 
